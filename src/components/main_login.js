b7b8a3356fcfdd6f45bb7c8c52ba9fa4b7c39ba9
Hoang Nguyen <hoang.nguyen1403@hcmut.edu.vn>
2021-05-05 00:49:10 +0700

    loginded component : Hoang Update

--- src/components/main_login.js
+++ src/components/main_login.js
@@ -4,11 +4,15 @@ import ReactDOM from 'react-dom';
 import './Semantic/semantic.min.css'
 import './style.css'
 import {Button, Modal} from 'semantic-ui-react'
-import Menubar from "./Menubar";
+import Logined_Menubar from "./logined_menubar";
+import Logined_Main from "./logined_main";
+
 const Main_login = (props) =>{
     return (
         <div className='ui'>
-            <Menubar/>
+            <Logined_Menubar/>
+            <div className="ui section divider"></div>
+            <Logined_Main/>
         </div>
     )
 }
